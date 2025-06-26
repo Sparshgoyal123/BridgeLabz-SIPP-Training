9271015e3a3bc66f4fdb546c4f32de9fa6e510f4
Sparshgoyal123 <133898205+Sparshgoyal123@users.noreply.github.com>
2025-06-26 10:24:12 +0530

    Added solution for printing Welcome to Bridge Labz, Day3.

--- PracticeProblems/PrintBridgeLabz.java
+++ PracticeProblems/PrintBridgeLabz.java
@@ -1,3 +1,8 @@
+/*
+Welcome to Bridgelabz!
+Write a program that prints "Welcome to Bridgelabz!" to the screen.
+*/
+
 public class PrintBridgeLabz {
     public static void main(String[] args) {
         System.out.println("Bridge Labz");
